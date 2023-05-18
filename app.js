0250a20e1abe46df8cbe524e80104feb2cb9f952
Ben-Hur <ben-hurqbeltrao@fac.pe.senac.br>
2023-05-18 17:54:18 -0300

    Alterações para acessar o Postman e adequar o Mongodb

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once ("open", () => { // abrir conexao com o banco
+
+    console.log('conexão com o banco feita com sucesso')
+})
 const app = express();
-
-app.use(express.json())
-
-const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobiit"}
-]
-// programa get - consulta/busca
-
-app.get('/', (req,res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
-})
-app.get('/livros/:id', (req, res) => { //:id para que ira servir para localizar o id e retorno no json
-    let index = buscaLivro(req.params.id);
-    //livros[index].titulo = req.body.titulo; Comentada porque essa linha substitui o titulo
-    res.json(livros[index]); //devolve na posicao que localizou
-
-})
-
-// programa post - criar/inserir
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body); //corp da requisição
-    res.status(201).send('Livro foi cadastrado com sucesso') // resposta que foi criado com sucesso
-})
-
-// programa para atualizar
-
-app.put('/livros/:id', (req, res) => { //:id para que ira servir para localizar o id e retorno no json
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-
-})
-
-// programa para deletar
-
-app.delete('/livros/:id', (req, res) => { //:id para que ira servir para localizar o id e retorno no json
-    let {id} = req.params;  // atribuição para 
-    let index = buscaLivro(id);
-    livros.splice(index, 1); // metodo splice localiza o livro pelo index e o segundo paramentro é a qtd que ira deletar
-    res.send(`Lvro ${id} removido com sucesso`);
-
-})
+app.use(express.json());
+routes(app);
 
 
-// funcao para localizar o livro pelo id
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id) // findIndex localizara o o indice que foi passado por parametro
-}
 
 export default app
