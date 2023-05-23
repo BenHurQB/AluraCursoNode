09e1ce7b65aaede046bd14d9a198c2e32a0b5d70
Ben-Hur <ben-hurqbeltrao@fac.pe.senac.br>
2023-05-23 18:22:54 -0300

    Curso Node - Melhorando o Codigo - Aula 01

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 
-
+import "dotenv/config"
 import app from './src/app.js'
 //const port = process.env.port || 3000;
-const port =  3000;
+const port = 3000;
+
 // listen é utilizado em servidores HTTP para escutar as requisições vindas de um determinado endereço de IP e porta
 // port indica a porta onde o servidor deve escutar as requisições e o segundo paramentro é uma função callback
 // que é executada qdo o servidor começa a escutar as requisições.
